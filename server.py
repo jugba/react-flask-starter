19d5e0c27619c8e05f4723027519df1cd8522f14
Joshua Ugba <justjosh001@gmail.com>
2020-05-01 22:45:33 +0100

    fixes path error

--- server.py
+++ server.py
@@ -1,6 +1,15 @@
+import logging
 from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='build', template_folder='build')             
+gunicorn_logger = logging.getLogger('gunicorn.error')
+
+
+app = Flask(__name__, 
+            static_folder='./build', 
+            template_folder='./build', 
+            static_url_path='/')             
+app.logger.handlers = gunicorn_logger.handlers
+app.logger.setLevel(gunicorn_logger.level)
 
 @app.route('/')                   
 def index():                      
